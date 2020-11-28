88b72a968ec4c6fcbb1c312498a2c38347b4b78f
Dominic <75138982+dosc91@users.noreply.github.com>
2020-11-28 15:33:28 +0100

    Add files via upload

--- R/LDLConvFunctions_data.R
+++ R/LDLConvFunctions_data.R
@@ -4,7 +4,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(grav)
+#' @usage data(LDLConvFunctions_data)
 #'
 #' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
 #'
@@ -14,4 +14,4 @@
 #'
 #' @references Baayen, R. H., Chuang, Y. Y., and Blevins, J. P. (2018). Inflectional morphology with linear mappings. The Mental Lexicon, 13 (2), 232-270.
 #' @references Chuang, Y-Y., Vollmer, M-l., Shafaei-Bajestan, E., Gahl, S., Hendrix, P., & Baayen, R. H. (2020). The processing of pseudoword form and meaning in production and comprehension: A computational modeling approach using Linear Discriminative Learning. Behavior Research Methods, 1-51.
-LDLConvFunctions_data.RData
+LDLConvFunctions_data
