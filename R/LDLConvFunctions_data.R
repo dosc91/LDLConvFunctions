935239abdc601587b03773f4e47811a8a1c5887c
Dominic <75138982+dosc91@users.noreply.github.com>
2020-11-28 15:28:03 +0100

    Add files via upload

--- R/LDLConvFunctions_data.R
+++ R/LDLConvFunctions_data.R
@@ -14,30 +14,4 @@
 #'
 #' @references Baayen, R. H., Chuang, Y. Y., and Blevins, J. P. (2018). Inflectional morphology with linear mappings. The Mental Lexicon, 13 (2), 232-270.
 #' @references Chuang, Y-Y., Vollmer, M-l., Shafaei-Bajestan, E., Gahl, S., Hendrix, P., & Baayen, R. H. (2020). The processing of pseudoword form and meaning in production and comprehension: A computational modeling approach using Linear Discriminative Learning. Behavior Research Methods, 1-51.
-
-#' @export
-
-ALC <- function (pseudo_S_matrix, real_S_matrix, pseudo_word_data)
-{
-  if (is.null(pseudo_S_matrix)) {
-    stop(call=F, geterrmessage = "pseudo_S_matrix not found\n")
-  }
-  if (is.null(real_S_matrix)) {
-    stop(call=F, geterrmessage = "real_S_matrix not found\n")
-  }
-  if (is.null(pseudo_word_data)) {
-    stop(call=F, geterrmessage = "pseudo_word_data not found\n")
-  }
-  results <- data.frame(matrix(ncol = 1, nrow = 0))
-  for (i in 1:length(row.names(pseudo_S_matrix)))
-  {
-    for (j in 1:length(row.names(real_S_matrix)))
-    {
-      results[i,j] <- cor(pseudo_S_matrix[i,], real_S_matrix[j,], method = "pearson")
-    }
-  }
-  ALC <- rowMeans(results)
-  Pseudoword <- pseudo_word_data$Word
-  Base <- pseudo_word_data$Base
-  ALCframe <- as.data.frame(cbind(ALC, Pseudoword, Base))
-}
+LDLConvFunctions_data.RData
